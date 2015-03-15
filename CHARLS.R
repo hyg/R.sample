99abc6ec6f0fb88d62ec1ceb916f417b73643388
Huang Yonggang <huangyg@mars22.com>
2015-03-15 18:04:37 +0800

    import charls data

---
+++ CHARLS.R
@@ -0,0 +1,4 @@
+options(max.print = 3000)
+library(foreign)
+#read.dta("there/is/your/file/directory/filename.dta",convert.factors=F)
+read.dta("C:/Users/huangyg/Desktop/charls/Demographic_Background.dta",convert.factors=F)
