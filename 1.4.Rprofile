ca724ed5b65f5eb022f0a98bc48e4a6ba83b5ed7
Huang Yonggang <huangyg@mars22.com>
2015-04-29 07:51:31 +0800

    4~5周任务

--- 1.4.Rprofile
+++ 1.4.Rprofile
@@ -1,5 +1,7 @@
 .First <- function(){
 library(foreign)
+options(max.print=300)
+
 cat("\nWelcome at ",date()," Let's get it done.\n")
 Sys.setenv(CHARLS_PATH="C:\\Users\\huangyg\\Desktop\\charls")
 cat("set environment variable CHARLS_PATH=",Sys.getenv("CHARLS_PATH"),"\n")
@@ -7,6 +9,10 @@ Sys.setenv(CGSS_PATH="C:\\Users\\huangyg\\Desktop\\CGSS")
 cat("set environment variable CGSS_PATH=",Sys.getenv("CGSS_PATH"),"\n")
 Sys.setenv(RSAMPLE_PATH="D:\\huangyg\\git\\R.sample")
 cat("set environment variable RSAMPLE_PATH=",Sys.getenv("RSAMPLE_PATH"),"\n")
+setwd("D:/huangyg/git/R.sample") #请改为本地路径
+cat("set working directory =",getwd(),"\n")
+source("func.R",encoding="utf-8") 
+cat("load func.R .\n")
 }
 
 .Last <- function(){
---
+++ func.R
@@ -0,0 +1,133 @@
+ch13 <- function(x) {
+  oldwd <- getwd()
+  setwd(Sys.getenv("CHARLS_PATH")) 
+  
+  n1 <- "Demographic_Background"
+  n2 <- "Family_Information"
+  n3 <- "Family_Transfer"
+  n4 <- "Health_Care_and_Insurance"
+  n5 <- "Health_Status_and_Functioning"
+  n6 <- "Household_Income"
+  n7 <- "Housing_Characteristics"
+  n8 <- "Individual_Income"
+  n9 <- "Interviewer_Observation"
+  n10 <- "PSU"
+  n11 <- "Weights"
+  n12 <- "Work_Retirement_and_Pension"
+  
+  ch13name <- c(n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12)
+  ch13filename <- paste("2013.CHARLS.Wave2/",ch13name,".dta",sep="")  
+  
+  ch13x <- read.dta(ch13filename[x],convert.factors=F)
+  
+  setwd(oldwd)
+  
+  return(ch13x)
+}
+
+ch13all <- function() {
+  oldwd <- getwd()
+  setwd(Sys.getenv("CHARLS_PATH")) 
+  
+  n1 <- "Demographic_Background"
+  n2 <- "Family_Information"
+  n3 <- "Family_Transfer"
+  n4 <- "Health_Care_and_Insurance"
+  n5 <- "Health_Status_and_Functioning"
+  n6 <- "Household_Income"
+  n7 <- "Housing_Characteristics"
+  n8 <- "Individual_Income"
+  n9 <- "Interviewer_Observation"
+  n10 <- "PSU"
+  n11 <- "Weights"
+  n12 <- "Work_Retirement_and_Pension"
+  
+  ch13name <- c(n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12)
+  ch13filename <- paste("2013.CHARLS.Wave2/",ch13name,".dta",sep="")  
+  
+  ch131 <- read.dta(ch13filename[1],convert.factors=F)
+  ch132 <- read.dta(ch13filename[2],convert.factors=F)
+  ch133 <- read.dta(ch13filename[3],convert.factors=F)
+  ch134 <- read.dta(ch13filename[4],convert.factors=F)
+  ch135 <- read.dta(ch13filename[5],convert.factors=F)
+  ch136 <- read.dta(ch13filename[6],convert.factors=F)
+  ch137 <- read.dta(ch13filename[7],convert.factors=F)
+  ch138 <- read.dta(ch13filename[8],convert.factors=F)
+  ch139 <- read.dta(ch13filename[9],convert.factors=F)
+  ch1310 <- read.dta(ch13filename[10],convert.factors=F)
+  ch1311 <- read.dta(ch13filename[11],convert.factors=F)
+  ch1312 <- read.dta(ch13filename[12],convert.factors=F)
+  
+  ch13list <- list(ch131,ch132,ch133,ch134,ch135,ch136,ch137,ch138,ch139,ch1310,ch1311,ch1312)
+  
+  setwd(oldwd)
+  
+  return(ch13list)
+}
+
+ch12 <- function(x) {
+  oldwd <- getwd()
+  setwd(Sys.getenv("CHARLS_PATH")) 
+  
+  n1 <- "Biomarkers"
+  n2 <- "Demographic_background"
+  n3 <- "Family_Information"
+  n4 <- "Family_Transfer"
+  n5 <- "Health_Care_and_Insurance"
+  n6 <- "Health_Status_and_Functioning"
+  n7 <- "Household_Income"
+  n8 <- "Household_Roster"
+  n9 <- "Housing_Characteristics"
+  n10 <- "Individual_Income"
+  n11 <- "Interviewer_Observation"
+  n12 <- "Work_Retirement_and_Pension"
+  
+  ch12name <- c(n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12)
+  ch12filename <- paste("2012.CHARLS.Pilot.Wave2/",ch12name,".dta",sep="")  
+  
+  ch12x <- read.dta(ch12filename[x],convert.factors=F)
+  
+  setwd(oldwd)
+  
+  return(ch12x)
+}
+
+ch12all <- function() {
+  oldwd <- getwd()
+  setwd(Sys.getenv("CHARLS_PATH")) 
+  
+  n1 <- "Biomarkers"
+  n2 <- "Demographic_background"
+  n3 <- "Family_Information"
+  n4 <- "Family_Transfer"
+  n5 <- "Health_Care_and_Insurance"
+  n6 <- "Health_Status_and_Functioning"
+  n7 <- "Household_Income"
+  n8 <- "Household_Roster"
+  n9 <- "Housing_Characteristics"
+  n10 <- "Individual_Income"
+  n11 <- "Interviewer_Observation"
+  n12 <- "Work_Retirement_and_Pension"
+  
+  ch12name <- c(n1,n2,n3,n4,n5,n6,n7,n8,n9,n10,n11,n12)
+  ch12filename <- paste("2012.CHARLS.Pilot.Wave2/",ch12name,".dta",sep="")  
+  
+  ch121 <- read.dta(ch12filename[1],convert.factors=F)
+  ch122 <- read.dta(ch12filename[2],convert.factors=F)
+  ch123 <- read.dta(ch12filename[3],convert.factors=F)
+  ch124 <- read.dta(ch12filename[4],convert.factors=F)
+  ch125 <- read.dta(ch12filename[5],convert.factors=F)
+  ch126 <- read.dta(ch12filename[6],convert.factors=F)
+  ch127 <- read.dta(ch12filename[7],convert.factors=F)
+  ch128 <- read.dta(ch12filename[8],convert.factors=F)
+  ch129 <- read.dta(ch12filename[9],convert.factors=F)
+  ch1210 <- read.dta(ch12filename[10],convert.factors=F)
+  ch1211 <- read.dta(ch12filename[11],convert.factors=F)
+  ch1212 <- read.dta(ch12filename[12],convert.factors=F)
+  
+  ch12list <- list(ch121,ch122,ch123,ch124,ch125,ch126,ch127,ch128,ch129,ch1210,ch1211,ch1212)
+  
+  setwd(oldwd)
+  
+  return(ch12list)
+}
